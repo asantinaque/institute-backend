a6dcb0f3d1d054fb99c396a77bfbaefa92cf0261
DUST <asantinaque@protonmail.com>
2021-03-02 18:55:29 -0300

    first commit

---
+++ src/main/java/com/asantinaque/backendinstitute/BackendInstituteApplication.java
@@ -0,0 +1,13 @@
+package com.asantinaque.backendinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendInstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendInstituteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asantinaque/backendinstitute/BackendInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asantinaque.backendinstitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendInstituteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
